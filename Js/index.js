db16ee291c530fe9eada027bcdf28a32149debdc
Marlos Santana <83357882+marlossantanaa@users.noreply.github.com>
2021-12-09 14:12:20 -0300

    StackX
    Projeto de FrontEnd

---
+++ Js/index.js
@@ -0,0 +1,13 @@
+//funcóe
+console.log('Hello World');
+let varS= '';
+let varE= 'D';
+let varY= 'U';
+let varZ;
+
+varZ=varY ;
+varY=varS ;
+varS=varE ;
+varE=varZ ;
+ console.log(varY+varE+varS);
+
